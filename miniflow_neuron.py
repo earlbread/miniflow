518b68e8cb5cc34047432d0b9ed1e65c68238122
Seunghun Lee <waydi1@gmail.com>
2016-12-24 12:35:52 -0800

    Rename miniflow to miniflow_neuron
    To add miniflow layer version, rename miniflow to miniflow_neuron

--- miniflow_neuron.py
+++ miniflow_neuron.py
@@ -72,6 +72,24 @@ class Add(Neuron):
             self.value += neuron.value
 
 
+class Linear(Neuron):
+    def __init__(self, inputs, weights, bias):
+        Neuron.__init__(self, inputs)
+        # NOTE: The weights and bias properties here are not
+        # numbers, but rather references to other neurons.
+        # The weight and bias values are stored within the
+        # respective neurons.
+        self.weights = weights
+        self.bias = bias
+
+    def forward(self):
+        """Set self.value to the value of the linear function output.
+        """
+        self.value = self.bias.value
+        for w, x in zip(self.weights, self.inbound_neurons):
+            self.value += w.value * x.value
+
+
 def topological_sort(feed_dict):
     """
     Sort the neurons in topological order using Kahn's Algorithm.
